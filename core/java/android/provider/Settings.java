30bd8b7bb55edc4dcac7ee562846ee0b05e711c6
cristianomatos <cristianobmatos@gmail.com>
2013-10-31 22:44:36 -0200

    Network stats custom colors (1/2)
    Change-Id: I5e23e9cde7b417480c8371f368cce18c9ea29c96

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3021,18 +3021,23 @@ public final class Settings {
          */
         public static final String STATUSBAR_PEEK = "statusbar_peek";
 
-	 /**
-          * Whether to show the network status in the status bar
-          * @hide
-          */
-         public static final String STATUS_BAR_NETWORK_STATS = "status_bar_network_stats";
+	/**
+         * Whether to show the network status in the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_NETWORK_STATS = "status_bar_network_stats";
  
-         /**
-          * Frequency at which stats are updated, in milliseconds
-          * @hide
-          */
-         public static final String STATUS_BAR_NETWORK_STATS_UPDATE_INTERVAL = "status_bar_network_stats_update_frequency";
+        /**
+         * Frequency at which stats are updated, in milliseconds
+         * @hide
+         */
+        public static final String STATUS_BAR_NETWORK_STATS_UPDATE_INTERVAL = "status_bar_network_stats_update_frequency";
 
+	/**
+         * Network stats Color style
+         * @hide
+         */
+        public static final String STATUS_BAR_NETWORK_COLOR = "status_bar_network_usage_color";
 
 	/**
          * Show the notification ticker (or not)

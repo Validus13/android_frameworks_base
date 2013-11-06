538a97b88f80aaf9c705c608682a597c1993fa6e
xplodwild <me@xplod.fr>
2013-11-06 06:25:06 -0200

    Settings: add HEADSET_CONNECT_PLAYER key
    Change-Id: Ic0b7d83f8ab7b8aacf2a7f5d6c3863e7c9edf0cd

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2406,6 +2406,12 @@ public final class Settings {
          */
         public static final String POINTER_SPEED = "pointer_speed";
 
+	/**
+	 * Whether or not to launch default music player when headset is connected
+	 * @hide
+	 */
+        public static final String HEADSET_CONNECT_PLAYER = "headset_connect_player";
+
         /**
          * I am the lolrus.
          * <p>
